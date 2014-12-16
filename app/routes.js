fea390efd0caf00adc80010e5da951f8b8d0487d
iamwave007 <iamwave007@hotmail.com>
2014-12-15 20:44:35 -0500

    finished first authentication

--- app/routes.js
+++ app/routes.js
@@ -8,45 +8,36 @@ module.exports = function(app,passport){
     res.render('index.ejs'); // load the index.ejs file
   });
 
-  // ====
-  // LOGIN ===
-  // ===
-  // show the login form
   app.get('/login',function(req,res){
     // render the page and pass in any flash data if exists
     res.render('login.ejs',{message: req.flash('loginMessage')});
 
   });
 
-  // process the login form
-  // app.post ('/login', do all our passport stuff here);
+  app.post('/login', passport.authenticate('local-login',{
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash : true
+  }));
 
-  //====
-  // SIGNUP ===
-  // ===
-  // show the signup form
   app.get('/signup',function(req,res){
     //render the page and pass in any flash data if exists
     res.render('signup.ejs',{message: req.flash('signupMessage')})
   });
 
-  // process the signup form
-  // app.post('/signup', do all our passport stuff here);
+  app.post('/signup',passport.authenticate('local-signup',{
+    successRedirect : '/profile',
+    failureRedirect : '/signup',
+    failureFlash : true
+  }));
 
-  // =====================================
-  // PROFILE SECTION =====================
-  // =====================================
-  // we will want this protected so you have to be logged in to visit
-  // we will use route middleware to verify this (the isLoggedIn function)
 
   app.get('/profile',isLoggedIn,function(req,res){
     res.render('profile.ejs',{
       user: req.user // get the user out of session and pass to template
     });
   });
-  // =====================================
-  // LOGOUT ==============================
-  // =====================================
+
 
   app.get('/logout',function(req,res){
     req.logout();
@@ -54,7 +45,6 @@ module.exports = function(app,passport){
   });
 };
 
-// route middleware to make sure a user is logged in
 
 function isLoggedIn(req,res,next){
   // if user is authenticated in the session, carry on
